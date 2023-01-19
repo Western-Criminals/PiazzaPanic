fe64081391e5ef025428e5e2d3fafb66c3c296fc
zatopulse <cf1217@york.ac.uk>
2023-01-19 16:01:00 +0000

    Deleted useless files

--- 12_Kitchen_Shadowless_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="12_Kitchen_Shadowless_32x32" tilewidth="32" tileheight="32" tilecount="784" columns="16">
- <image source="../../../../../../Downloads/Modern_Interiors_v41.2_2/1_Interiors/32x32/Theme_Sorter_Shadowless_32x32/12_Kitchen_Shadowless_32x32.png" width="512" height="1568"/>
-</tileset>
