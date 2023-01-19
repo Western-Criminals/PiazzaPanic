7e9de63903f3396adaa4e53e5ab7b2859f83f697
zatopulse <cf1217@york.ac.uk>
2023-01-19 13:54:07 +0000

    Added hit detection

--- piazzaPanic/core/src/com/westerncriminals/game/screens/PlayScreen.java
+++ piazzaPanic/core/src/com/westerncriminals/game/screens/PlayScreen.java
@@ -5,13 +5,25 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.westerncriminals.game.PiazzaPanic;
 import com.westerncriminals.game.scenes.Hud;
+
+import sprites.Chef;
 
 public class PlayScreen implements Screen{
 	private PiazzaPanic game;
@@ -19,10 +31,15 @@ public class PlayScreen implements Screen{
 	private Viewport gamePort;
 	private Hud hud;
 	private String hello;
+	private Chef chefOne;
+	
 	
 	private TmxMapLoader maploader;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
+    
+    private World world;
+    private Box2DDebugRenderer b2dr;
 	
 	public PlayScreen(PiazzaPanic game) {
 		this.game = game;
@@ -32,10 +49,46 @@ public class PlayScreen implements Screen{
 		gamePort.apply();
 		hud = new Hud(game.batch);
 		
+		
 		maploader = new TmxMapLoader();
         map = maploader.load("finalKitchen.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
         gamecam.position.set(gamePort.getWorldWidth() / 2 , gamePort.getWorldHeight() / 2 , 0);
+        
+        world = new World(new Vector2(0,0), true);
+        b2dr = new Box2DDebugRenderer();
+        chefOne = new Chef(world);
+        
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+        
+        for(MapObject object: map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+        	Rectangle rect = ((RectangleMapObject) object).getRectangle();
+        	
+        	bdef.type = BodyDef.BodyType.StaticBody;
+        	bdef.position.set(rect.getX() + rect.getWidth() /2, rect.getY() + rect.getHeight() /2);
+        	
+        	body = world.createBody(bdef);
+        	
+        	shape.setAsBox(rect.getWidth()/ 2, rect.getHeight()/ 2);
+        	fdef.shape = shape;
+        	body.createFixture(fdef);
+        }
+        
+        for(MapObject object: map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)){
+        	Rectangle rect = ((RectangleMapObject) object).getRectangle();
+        	
+        	bdef.type = BodyDef.BodyType.StaticBody;
+        	bdef.position.set(rect.getX() + rect.getWidth() /2, rect.getY() + rect.getHeight() /2);
+        	
+        	body = world.createBody(bdef);
+        	
+        	shape.setAsBox(rect.getWidth()/ 2, rect.getHeight()/ 2);
+        	fdef.shape = shape;
+        	body.createFixture(fdef);
+        }
         
 	}
 
@@ -47,6 +100,9 @@ public class PlayScreen implements Screen{
 	
 	public void update(float dt) {
 		handleInput();
+		
+		world.step(1/60f, 6, 2);
+		
 		gamecam.update();
 		renderer.setView(gamecam);
 	}
@@ -63,6 +119,8 @@ public class PlayScreen implements Screen{
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		renderer.render();
+		
+		 b2dr.render(world, gamecam.combined);
 		
 		game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
 		hud.stage.draw();
---
+++ piazzaPanic/core/src/sprites/Chef.java
@@ -0,0 +1,33 @@
+package sprites;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Chef extends Sprite{
+	public World world;
+	public Body b2body;
+	
+	public Chef(World world) {
+		this.world = world;
+		defineChef();
+	}
+	
+	public void defineChef() {
+		BodyDef bdef = new BodyDef();
+		bdef.position.set(32,32);
+		bdef.type = BodyDef.BodyType.DynamicBody;
+		b2body = world.createBody(bdef);
+		
+		FixtureDef fdef = new FixtureDef();
+		CircleShape shape = new CircleShape();
+		shape.setRadius(10);
+		
+		fdef.shape = shape;
+		b2body.createFixture(fdef);
+		
+	}
+}
