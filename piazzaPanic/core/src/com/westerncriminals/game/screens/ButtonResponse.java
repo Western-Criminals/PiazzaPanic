bee3d5eb747cc9a7619d1535c69528c3a85d4fd2
Dan <d.elia@outlook.com>
2023-01-20 16:43:50 +0000

    tryna fix the menu

--- piazzaPanic/core/src/com/westerncriminals/game/screens/ButtonResponse.java
+++ piazzaPanic/core/src/com/westerncriminals/game/screens/ButtonResponse.java
@@ -1,13 +1,15 @@
 package com.westerncriminals.game.screens;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Game;
+import com.westerncriminals.game.PiazzaPanic;
 
 public interface ButtonResponse {
         public void execute();
     }
 class GameStart implements ButtonResponse {
+    private static PiazzaPanic game;
     public void execute() {
-
-        //
+        game.setScreen(new PlayScreen(game));
     }
 }
 class GameExit implements ButtonResponse {
