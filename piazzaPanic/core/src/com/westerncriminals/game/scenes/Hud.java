2a50940c8766ae7b4831b48fd0fb152c36a7f652
ZatoPulse <chrisfoister@outlook.com>
2023-01-23 21:18:01 +0000

    Refactored more code
    added hit detection with fryer using world listener

--- piazzaPanic/core/src/com/westerncriminals/game/scenes/Hud.java
+++ piazzaPanic/core/src/com/westerncriminals/game/scenes/Hud.java
@@ -11,27 +11,31 @@ import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.westerncriminals.game.PiazzaPanic;
+import com.westerncriminals.game.sprites.Chef;
 
 public class Hud implements Disposable{
 	public Stage stage;
 	private Viewport viewport;
 	
-	private Integer scoreCount;
+	private float timeCount;
 	private Integer bCount;
 	private Integer saladCount;
+	private Integer worldTime;
 
 	
-	Label scoreLabel;
+	Label timerLabel;
 	Label bLabelNum;
 	Label burgLabel;
 	Label timeLabel;
 	Label saladLabel;
 	Label sLabel;
+	Label ingrediantLabel;
 	
 	public Hud(SpriteBatch sb) {
-		scoreCount = 0;
-		setbCount(0);
+		timeCount = 0;
+		bCount = 0;
 		saladCount = 0;
+		worldTime = 0;
 		
 		viewport = new FitViewport(PiazzaPanic.V_WIDTH, PiazzaPanic.V_HEIGHT, new OrthographicCamera());
 		stage = new Stage(viewport, sb);
@@ -43,24 +47,40 @@ public class Hud implements Disposable{
 		burgLabel = new Label("Burgers", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
 		timeLabel = new Label("TIME", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
 		saladLabel = new Label("Salads", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
 		
 		table.add(timeLabel).expandX();
 		table.add(burgLabel).expandX();
 		table.add(saladLabel).expandX();
 		
+		
 		table.row();
 		
-		scoreLabel = new Label(String.format("%06d",scoreCount), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-		bLabelNum = new Label(String.format("%01d", getbCount()), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+		timerLabel = new Label(String.format("%06d",worldTime), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+		bLabelNum = new Label(String.format("%01d", bCount), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
 		sLabel = new Label(String.format("%01d", saladCount), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+		ingrediantLabel = new Label(String.format("%01d", 0), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
 		
-		table.add(scoreLabel).expandX();//.padTop(10);
+		table.add(timerLabel).expandX();//.padTop(10);
 		table.add(bLabelNum).expandX();//.padTop(10);
 		table.add(sLabel);
 		
+		table.row();
+		
+		table.add(ingrediantLabel);
+		
+		
 		stage.addActor(table);
 		
-		
+	}
+	
+	public void update(float dt) {
+		timeCount += dt;
+		if(timeCount >= 1) {
+			worldTime++;
+			timerLabel.setText(String.format("%06d",(worldTime)));
+			timeCount = 0;
+		}
 	}
 
 	@Override
@@ -70,11 +90,4 @@ public class Hud implements Disposable{
 		
 	}
 
-	public Integer getbCount() {
-		return bCount;
-	}
-
-	public void setbCount(Integer bCount) {
-		this.bCount = bCount;
-	}
 }
