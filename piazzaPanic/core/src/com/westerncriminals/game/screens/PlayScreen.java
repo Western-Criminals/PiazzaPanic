fa91206f275de2a9cb230a9dd83edc98f7533de9
Dipken Fung <23611770+LuciferLordKing@users.noreply.github.com>
2023-01-31 22:25:00 +0000

    Update PlayScreen.java
    pain

--- piazzaPanic/core/src/com/westerncriminals/game/screens/PlayScreen.java
+++ piazzaPanic/core/src/com/westerncriminals/game/screens/PlayScreen.java
@@ -29,6 +29,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PlayScreen implements Screen{
 	JSONObject settings;
@@ -136,7 +137,11 @@ public class PlayScreen implements Screen{
 			inv.setVisibility(!(inv.getVisibility()));
 			if (inv.getVisibility()) {
 				Gdx.app.log("I", (String) chefOne.itemStack.pop());
-				//inv.setInv(new ArrayList<String>()); // Fill dis shit up bruh
+				List<String> inv_lst = new ArrayList<String>();
+				while (chefOne.itemStack.notEmpty()) {
+					inv_lst.add(chefOne.itemStack.pop().toString());
+				}
+				inv.setInv(inv_lst);
 			}
 		}
 	}
