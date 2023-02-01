5d9be3c0e8696c27085227295ed23730c699b974
Dipken Fung <23611770+LuciferLordKing@users.noreply.github.com>
2023-02-01 06:58:30 +0000

    Update Customer.java
    pain

--- piazzaPanic/core/src/com/westerncriminals/game/sprites/Customer.java
+++ piazzaPanic/core/src/com/westerncriminals/game/sprites/Customer.java
@@ -1,55 +1,48 @@
 package com.westerncriminals.game.sprites;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 import com.westerncriminals.game.PiazzaPanic;
 import com.westerncriminals.game.screens.PlayScreen;
 
-public class Customer extends NPC{
+public class Customer extends NPC {
 
-	private float stateTime;
-	public Array<Object> itemStack;
-	private TextureRegion chefIdle;
-	public boolean customerPresent;
+    private float stateTime;
+    public Array<Object> itemStack;
+    private TextureRegion customerIdle;
+    public boolean customerPresent;
+    float startX, startY;
 
-	
-	public Customer(PlayScreen screen, float x, float y ) {
-		super(screen, x , y);
-		defineNPC();
-		chefIdle = new TextureRegion(screen.getAtlas().findRegion("Chef A1"),21, 9, 9,17); 
-		stateTime = 0;
+    public Customer(PlayScreen screen, float x, float y, String imgPath) {
+        super(screen, x , y);
+        defineNPC();
+        customerIdle = new TextureRegion(screen.getAtlas().findRegion("Chef A1"),21, 9, 9,17);
+        stateTime = 0;
         setBounds(getX(), getY(), 20f/ PiazzaPanic.PPM, 34f / PiazzaPanic.PPM);
-        setRegion(chefIdle);
-		itemStack = new Array<Object>();
-		customerPresent = false;
-	}
-	
+        setRegion(customerIdle);
+        itemStack = new Array<Object>();
+        customerPresent = false;
+    }
 
-	 public void update(float dt){
-		 stateTime += dt;
-		 if (!customerPresent) {
-			 //world.destroyBody(b2body);
-			 stateTime = 0;
-		 }
-		 else if (customerPresent)
-	        b2body.setLinearVelocity(velocity);
-	        setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
-	    }
-	
-
-	@Override
-	protected void defineNPC() {
-		BodyDef bdef = new BodyDef();
-		bdef.position.set((((float) getX()) + 55) / PiazzaPanic.PPM, (((float) getY()) -35) / PiazzaPanic.PPM);
+    public void update(float dt) {
+        stateTime += dt;
+        if (!customerPresent) {
+            //world.destroyBody(b2body);
+            stateTime = 0;
+        }
+        else if (customerPresent)
+            b2body.setLinearVelocity(velocity);
+        setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
+        // may need to create a new fixture for contact, maybe
+    }
+    @Override
+    protected void defineNPC() {
+        BodyDef bdef = new BodyDef();
+        bdef.position.set((((float) getX()) + 55) / PiazzaPanic.PPM, (((float) getY()) -35) / PiazzaPanic.PPM);
         bdef.type = BodyDef.BodyType.DynamicBody;
         b2body = world.createBody(bdef);
 
@@ -61,10 +54,10 @@ public class Customer extends NPC{
 
         fdef.shape = shape;
         b2body.createFixture(fdef).setUserData(this);
-	}
-	
-	public void draw(Batch batch){
-		if (customerPresent)
-			super.draw(batch);
+    }
+
+    public void walkTowardCounter(Batch batch) {
+        if (customerPresent)
+            super.draw(batch);
     }
 }
