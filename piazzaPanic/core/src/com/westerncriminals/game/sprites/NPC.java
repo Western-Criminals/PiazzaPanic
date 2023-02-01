94da1e0306817d8fe1e5167d6a1bb6e2a2e7576b
Dipken Fung <23611770+LuciferLordKing@users.noreply.github.com>
2023-02-01 06:33:56 +0000

    Add files via upload
    pain

---
+++ piazzaPanic/core/src/com/westerncriminals/game/sprites/NPC.java
@@ -0,0 +1,33 @@
+package com.westerncriminals.game.sprites;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
+import com.westerncriminals.game.screens.PlayScreen;
+
+public abstract class NPC extends Sprite{
+
+    protected World world;
+    protected PlayScreen screen;
+    public Body b2body;
+    public Vector2 velocity;
+
+    public NPC(PlayScreen screen, float x, float y){
+        this.world = screen.getWorld();
+        this.screen = screen;
+        setPosition(x, y);
+        defineNPC();
+        velocity = new Vector2(0, 1);
+    }
+
+    protected abstract void defineNPC();
+    public abstract void update(float dt);
+
+    public void reverseVelocity(boolean x, boolean y) {
+        if(x)
+            velocity.x = -velocity.x;
+        if(y)
+            velocity.y = -velocity.y;
+    }
+}
