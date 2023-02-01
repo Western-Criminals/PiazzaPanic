4ea9ebcfb9db75bb8a7a055cab9519c664924ea0
Dipken Fung <23611770+LuciferLordKing@users.noreply.github.com>
2023-02-01 07:24:07 +0000

    Update WorldContactListener.java
    pain

--- piazzaPanic/core/src/com/westerncriminals/game/tools/WorldContactListener.java
+++ piazzaPanic/core/src/com/westerncriminals/game/tools/WorldContactListener.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import com.westerncriminals.game.sprites.InteractiveTileObject;
+import com.westerncriminals.game.sprites.NPC;
+import com.westerncriminals.game.PiazzaPanic;
 import com.westerncriminals.game.sprites.BurgerFryer;
 
 public class WorldContactListener implements ContactListener{
@@ -17,6 +19,8 @@ public class WorldContactListener implements ContactListener{
 
 		Fixture fixA = contact.getFixtureA();
 		Fixture fixB = contact.getFixtureB();
+
+		int cDef = fixA.getFilterData().categoryBits | fixB.getFilterData().categoryBits;
 
 		if(fixA.getUserData() == "chefOne" || fixB.getUserData() == "chefOne") {
 			Fixture chefBody = fixA.getUserData() == "chefOne" ? fixA : fixB;
@@ -36,7 +40,17 @@ public class WorldContactListener implements ContactListener{
 			}
 		}
 
+		switch (cDef) {
+			case PiazzaPanic.CATEGORY_CUSTOMER | PiazzaPanic.CATEGORY_COUNTER:
+				if (fixA.getFilterData().categoryBits == PiazzaPanic.CATEGORY_CUSTOMER) {
+					((NPC) fixA.getUserData()).reverseVelocity(false, true);
+				}
+				else
+					((NPC )fixB.getUserData()).reverseVelocity(false, true);
+		}
+
 	}
+
 
 	@Override
 	public void endContact(Contact contact) {
@@ -55,5 +69,4 @@ public class WorldContactListener implements ContactListener{
 		// TODO Auto-generated method stub
 
 	}
-
 }
